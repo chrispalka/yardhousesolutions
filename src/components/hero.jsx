a4bd33571c6f8f70cdd9a04bfa35b2d4aedbda9f
Chris Palka <cpalka87@gmail.com>
2025-01-17 15:04:42 -0700

    Optimize hero image

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,7 @@
-import HeroImg from '../assets/nursehelpelderly.webp';
+import HeroImgLg from '../assets/nursehelpelderly.webp';
+import HeroImg320 from '../assets/nursehelpelderly-320.webp';
+import HeroImg768 from '../assets/nursehelpelderly-768.webp';
+import HeroImg1280 from '../assets/nursehelpelderly-1280.webp';
 import styles from '../assets/styles/Hero.module.css';
 
 function Hero() {
@@ -19,7 +22,19 @@ function Hero() {
           </div>
         </div>
         <div className={styles.gradient}>
-          <img src={HeroImg} className={styles.heroImage} alt='' />
+          <img
+            src={HeroImgLg}
+            className={styles.heroImage}
+            srcSet={`
+        ${HeroImg320} 320w,
+        ${HeroImg768} 768w,
+        ${HeroImg1280} 1280w
+      `}
+            sizes='(max-width: 600px) 100vw,
+             (max-width: 1200px) 50vw,
+             33vw'
+            alt='Nurse helping an elderly person'
+          />
         </div>
       </div>
       <div className={styles.telephoneContainer}>
